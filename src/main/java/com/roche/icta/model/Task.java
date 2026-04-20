0f60ee971528e99ce0c31bd447db15a8f9748859
spichigm <mara.spichiger@roche.com>
2026-04-20 12:57:05 +0200

    feat: alle classes

---
+++ src/main/java/com/roche/icta/model/Task.java
@@ -0,0 +1,16 @@
+package com.roche.icta.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tasks")
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
