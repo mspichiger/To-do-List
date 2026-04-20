a1c790001d86c25dde5b44b415df62eb21380002
spichigm <mara.spichiger@roche.com>
2026-04-20 12:56:21 +0200

    feat: import todo application

--- src/main/java/com/roche/icta/TodoApplication.java
+++ src/main/java/com/roche/icta/TodoApplication.java
@@ -1,1 +1,11 @@
 package com.roche.icta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+}
