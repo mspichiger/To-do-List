eeb166f3aa3936feaf89c6feb741bb794ad6c3ec
spichigm <mara.spichiger@roche.com>
2026-04-23 14:47:31 +0200

    fix: back to jpa repository

--- src/main/java/com/roche/icta/repository/TaskRepository.java
+++ src/main/java/com/roche/icta/repository/TaskRepository.java
@@ -2,9 +2,9 @@ package com.roche.icta.repository;
 
 import com.roche.icta.model.Task;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
